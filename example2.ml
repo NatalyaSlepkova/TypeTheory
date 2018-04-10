28bc07f8399a441f467ca15da19115d7f532c9d8
NatalyaSlepkova <slepkovanatal@mail.ru>
2018-04-10 20:27:15 +0300

    Angstrom code

---
+++ example2.ml
@@ -0,0 +1,27 @@
+open Angstrom
+open Printf
+
+type algebraic_term = Var of string | Fun of string * (algebraic_term list)
+
+let rec list_to_string x str = match x with
+        |[]-> str
+        |h::t -> type_to_string h (" " ^  (list_to_string t str))
+
+and type_to_string x str = match x with 
+        |Var a -> a ^ " " ^ str
+        |Fun(a, b) ->  a ^ " (" ^ (list_to_string b str) ^ ")" ;;
+
+let identifier =
+    take_while1 (function 'a' .. 'z' -> true | '0' .. '9' -> true | _ -> false)
+let variable = identifier >>| (fun x -> Var x)
+let termList = fix (fun m -> 
+  let functionCall = lift4 (fun idf s1 terms s2 -> Fun (idf, terms)) identifier (char '(') m (char ')') in
+  let term = functionCall
+  in sep_by (char ',') term)
+let eval2 (str:string) =
+  match parse_only termList (`String str) with
+  | Result.Ok v      -> v
+  | Result.Error msg -> failwith msg
+
+let a = eval2 "a8(b5()),c7()";;
+print_string (list_to_string a "");; 
